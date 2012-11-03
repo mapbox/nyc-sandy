a41b043c846242f4842641fee358f7c357759711
Alex Barth <alex@developmentseed.org>
2012-11-03 19:05:49 -0400

    Latest update.

--- js/centers.js
+++ js/centers.js
@@ -1,8 +1,10 @@
-// javascript:var%20centers%20=%20{%22type%22:%20%22FeatureCollection%22,%22features%22:%20[{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22Q505%22,%22BLDG_NAME%22:%20%22HILLCREST%20HS%20-%20Q%22,%22BLDG_ADD%22:%20%22160-05%20HIGHLAND%20AVENUE%22,%22CITY%22:%20%22JAMAICA%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2011432.0,%22BORO%22:%20%22QN%22,%22OEM_LABEL%22:%20%22HILLCREST%20HS%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.802579079762552,%2040.709673388373837]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22M625%22,%22BLDG_NAME%22:%20%22HS%20OF%20GRAPHIC%20COMMUNICATION%20ARTS-M%22,%22BLDG_ADD%22:%20%22439%20WEST%2049%20STREET%22,%22CITY%22:%20%22NEW%20YORK%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2010019.0,%22BORO%22:%20%22MN%22,%22OEM_LABEL%22:%20%22HS%20OF%20GRAPHIC%20COMM%20ARTS%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.99072122899166,%2040.763694115908457]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22K460%22,%22BLDG_NAME%22:%20%22JOHN%20JAY%20HS%20-%20K%22,%22BLDG_ADD%22:%20%22237%207TH%20AVENUE%22,%22CITY%22:%20%22BROOKLYN%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2011215.0,%22BORO%22:%20%22BK%22,%22OEM_LABEL%22:%20%22JOHN%20JAY%20HS%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.978856011518957,%2040.669393014974773]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22K430%22,%22BLDG_NAME%22:%20%22BROOKLYN%20TECH%20HS%20-%20K%22,%22BLDG_ADD%22:%20%2229%20FT%20GREENE%20PLACE%22,%22CITY%22:%20%22BROOKLYN%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2011217.0,%22BORO%22:%20%22BK%22,%22OEM_LABEL%22:%20%22BROOKLYN%20TECH%20HS%22,%22Role%22:%20%22Special%20Medical%20Needs%20Only%22},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.976453038660026,%2040.689009323661253]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22C001%22,%22BLDG_NAME%22:%20%22LEHMAN%20COLLEGE%22,%22BLDG_ADD%22:%20%22250%20BEDFORD%20PARK%20BLVD%20W%22,%22CITY%22:%20%22BRONX%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2010468.0,%22BORO%22:%20%22BX%22,%22OEM_LABEL%22:%20%22LEHMAN%20COLLEGE%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.89300738333921,%2040.874668278654681]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22K505%22,%22BLDG_NAME%22:%20%22FRANKLIN%20D.%20ROOSEVELT%20HS%20-%20K%22,%22BLDG_ADD%22:%20%225800%2020%20AVENUE%22,%22CITY%22:%20%22BROOKLYN%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2011204.0,%22BORO%22:%20%22BK%22,%22OEM_LABEL%22:%20%22FRANKLIN%20D.%20ROOSEVELT%20HS%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.981854808431635,%2040.620904538082897]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22M465%22,%22BLDG_NAME%22:%20%22G.%20WASHINGTON%20HS%20EDUC.%20CAMPUS%20-%20M%22,%22BLDG_ADD%22:%20%22549%20AUDOBON%20AVENUE%22,%22CITY%22:%20%22NEW%20YORK%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2010040.0,%22BORO%22:%20%22MN%22,%22OEM_LABEL%22:%20%22G.%20WASHINGTON%20HS%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.926227300598484,%2040.855600640087694]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22C005%22,%22BLDG_NAME%22:%20%22HUNTER%20COLLEGE%22,%22BLDG_ADD%22:%20%22695%20PARK%20AVE%22,%22CITY%22:%20%22NEW%20YORK%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2010065.0,%22BORO%22:%20%22MN%22,%22OEM_LABEL%22:%20%22HUNTER%20COLLEGE%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.965036379784308,%2040.76869789316325]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22C004%22,%22BLDG_NAME%22:%20%22JOHN%20JAY%20COLLEGE%22,%22BLDG_ADD%22:%20%22445%20W%2059TH%20ST%22,%22CITY%22:%20%22NEW%20YORK%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2010019.0,%22BORO%22:%20%22MN%22,%22OEM_LABEL%22:%20%22JOHN%20JAY%20COLLEGE%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.9866465864297,%2040.770356916551258]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22R455%22,%22BLDG_NAME%22:%20%22TOTTENVILLE%20HS%20-%20S.%20I.%22,%22BLDG_ADD%22:%20%22100%20LUTEN%20AVENUE%22,%22CITY%22:%20%22STATEN%20ISLAND%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2010312.0,%22BORO%22:%20%22SI%22,%22OEM_LABEL%22:%20%22TOTTENVILLE%20HS%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-74.192576569628187,%2040.527139316918543]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22R460%22,%22BLDG_NAME%22:%20%22SUSAN%20E.%20WAGNER%20HS%20-%20S.%20I.%22,%22BLDG_ADD%22:%20%221200%20MANOR%20ROAD%22,%22CITY%22:%20%22STATEN%20ISLAND%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2010314.0,%22BORO%22:%20%22SI%22,%22OEM_LABEL%22:%20%22SUSAN%20E.%20WAGNER%20HS%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-74.125250242743334,%2040.597669859068091]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22M445%22,%22BLDG_NAME%22:%20%22SEWARD%20PARK%20HS%20-%20MANHATTAN%22,%22BLDG_ADD%22:%20%22350%20GRAND%20STREET%22,%22CITY%22:%20%22NEW%20YORK%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2010002.0,%22BORO%22:%20%22MN%22,%22OEM_LABEL%22:%20%22SEWARD%20PARK%20HS%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.989354098807198,%2040.717311163647729]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22C010%22,%22BLDG_NAME%22:%20%22YORK%20COLLEGE%22,%22BLDG_ADD%22:%20%2294-20%20GUY%20R%20BREWER%20BLVD%22,%22CITY%22:%20%22JAMAICA%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2011451.0,%22BORO%22:%20%22QN%22,%22OEM_LABEL%22:%20%22YORK%20COLLEGE%22,%22Role%22:%20null},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.796143824376031,%2040.701788940627381]}},%20{%22type%22:%20%22Feature%22,%22properties%22:%20{%22BLDG_ID%22:%20%22P004%22,%22BLDG_NAME%22:%20%22PARK%20SLOPE%20ARMORY%22,%22BLDG_ADD%22:%20%221402%208%20AVENUE%22,%22CITY%22:%20%22BROOKLYN%22,%22STATE%22:%20%22NY%22,%22ZIP_CODE%22:%2011215.0,%22BORO%22:%20%22BK%22,%22OEM_LABEL%22:%20%22PARK%20SLOPE%20ARMORY%22,%22Role%22:%20%22Special%20Medical%20Needs%20Only%22},%22geometry%22:%20{%22type%22:%20%22Point%22,%22coordinates%22:%20[-73.982423025864932,%2040.662642579247134]}}]};
 var centers = {
+    "type": "FeatureCollection",
+
     "features": [{
         "type": "Feature",
         "properties": {
+            "OBJECTID": 1,
             "BLDG_ID": "Q505",
             "BLDG_NAME": "HILLCREST HS - Q",
             "BLDG_ADD": "160-05 HIGHLAND AVENUE",
@@ -20,6 +22,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 2,
             "BLDG_ID": "M625",
             "BLDG_NAME": "HS OF GRAPHIC COMMUNICATION ARTS-M",
             "BLDG_ADD": "439 WEST 49 STREET",
@@ -37,6 +40,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 3,
             "BLDG_ID": "K460",
             "BLDG_NAME": "JOHN JAY HS - K",
             "BLDG_ADD": "237 7TH AVENUE",
@@ -54,6 +58,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 4,
             "BLDG_ID": "K430",
             "BLDG_NAME": "BROOKLYN TECH HS - K",
             "BLDG_ADD": "29 FT GREENE PLACE",
@@ -71,6 +76,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 5,
             "BLDG_ID": "C001",
             "BLDG_NAME": "LEHMAN COLLEGE",
             "BLDG_ADD": "250 BEDFORD PARK BLVD W",
@@ -88,6 +94,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 6,
             "BLDG_ID": "K505",
             "BLDG_NAME": "FRANKLIN D. ROOSEVELT HS - K",
             "BLDG_ADD": "5800 20 AVENUE",
@@ -105,6 +112,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 7,
             "BLDG_ID": "M465",
             "BLDG_NAME": "G. WASHINGTON HS EDUC. CAMPUS - M",
             "BLDG_ADD": "549 AUDOBON AVENUE",
@@ -122,6 +130,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 8,
             "BLDG_ID": "C005",
             "BLDG_NAME": "HUNTER COLLEGE",
             "BLDG_ADD": "695 PARK AVE",
@@ -139,6 +148,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 9,
             "BLDG_ID": "C004",
             "BLDG_NAME": "JOHN JAY COLLEGE",
             "BLDG_ADD": "445 W 59TH ST",
@@ -156,6 +166,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 10,
             "BLDG_ID": "R455",
             "BLDG_NAME": "TOTTENVILLE HS - S. I.",
             "BLDG_ADD": "100 LUTEN AVENUE",
@@ -173,6 +184,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 11,
             "BLDG_ID": "R460",
             "BLDG_NAME": "SUSAN E. WAGNER HS - S. I.",
             "BLDG_ADD": "1200 MANOR ROAD",
@@ -190,6 +202,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 12,
             "BLDG_ID": "M445",
             "BLDG_NAME": "SEWARD PARK HS - MANHATTAN",
             "BLDG_ADD": "350 GRAND STREET",
@@ -207,6 +220,7 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 13,
             "BLDG_ID": "C010",
             "BLDG_NAME": "YORK COLLEGE",
             "BLDG_ADD": "94-20 GUY R BREWER BLVD",
@@ -224,6 +238,25 @@ var centers = {
     }, {
         "type": "Feature",
         "properties": {
+            "OBJECTID": 14,
+            "BLDG_ID": "C008",
+            "BLDG_NAME": "QUEENS COLLEGE",
+            "BLDG_ADD": "65-30 KISSENA BLVD",
+            "CITY": "FLUSHING",
+            "STATE": "NY",
+            "ZIP_CODE": 11367.0,
+            "BORO": "QN",
+            "OEM_LABEL": "QUEENS COLLEGE",
+            "Role": null
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-73.815595100225167, 40.737766094544092]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {
+            "OBJECTID": 15,
             "BLDG_ID": "P004",
             "BLDG_NAME": "PARK SLOPE ARMORY",
             "BLDG_ADD": "1402 8 AVENUE",
@@ -238,5 +271,7 @@ var centers = {
             "type": "Point",
             "coordinates": [-73.982423025864932, 40.662642579247134]
         }
-    }]
+    }
+
+    ]
 };
